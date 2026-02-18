30241c11b69870a173c30a08f67c2b2fe4e3c522
Om Satish Taywade <omstaywade@eng.rizvi.edu.in>
2026-02-18 15:51:41 +0530

    Update task15.py

--- task15.py
+++ task15.py
@@ -1,1 +1,20 @@
-// pass
+// Prime Gap Finder
+def isPrime(n):
+    for i in range(2, int(n**0.5)+1):
+        if(n%i == 0):
+            return False;
+        
+    return True
+
+num = int(input("Enter the number: "))
+print(isPrime(num))
+ans = 0
+count = 0
+while(count < 2):
+    num += 1
+    if(isPrime(num)):
+        count += 1
+        print(num)
+        ans = num - ans
+       
+print(ans)
